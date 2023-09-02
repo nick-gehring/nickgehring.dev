d044a659aec15665514d116df2443cc4a4c25b06
Nick Gehring <nick@eleventygroup.com>
2023-09-02 18:40:53 -0400

    tweak styles for posts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: ["wp-block-button__link", "mb-10"],
   theme: {
     fontFamily: {
       sans: ["Montserrat", "Trebuchet MS", "Helvetica", "sans-serif"],
